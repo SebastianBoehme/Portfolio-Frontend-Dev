6eb19c6718ab3f6e90d1bf271bd7e6715dee7665
Sebastian Böhme <sebastianboehme92@gmail.com>
2025-09-15 14:15:27 +0200

    Deleted temporarly unused component from template and added contact component. Also desidgned first part of contacts first content.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,13 +6,13 @@ import { NavigationComponent } from '../app/navigation/navigation.component';
 import { AboutMeComponent } from "../app/about-me/about-me.component";
 import { SkillsComponent } from "./skills/skills.component";
 import { ProjectsComponent } from "./projects/projects.component";
-import { ReferencesComponent } from "./references/references.component";
+import { ContactComponent } from "./contact/contact.component";
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, HeroSectionComponent, NavigationComponent, AboutMeComponent, SkillsComponent, ProjectsComponent, ReferencesComponent],
+  imports: [CommonModule, RouterOutlet, HeroSectionComponent, NavigationComponent, AboutMeComponent, SkillsComponent, ProjectsComponent, ContactComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
