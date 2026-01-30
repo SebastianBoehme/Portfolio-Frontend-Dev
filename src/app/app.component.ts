700ae361afa51099dac3645f793fadd42f9571e5
Sebastian Böhme <sebastianboehme92@gmail.com>
2026-01-30 16:28:50 +0100

    Added a glowing shadow around the cursor that follows the cursor without disturbing interactions with buttons and links

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, signal } from '@angular/core'; // signal & HostListener hinzugefügt
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { NavigationComponent } from '../app/shared/navigation/navigation.component';
-
 import { FooterComponent } from '../app/shared/footer/footer.component';
 
 @Component({
@@ -14,4 +13,16 @@ import { FooterComponent } from '../app/shared/footer/footer.component';
 })
 export class AppComponent {
   title = 'Portfolio';
+
+  // Wir erstellen zwei Signals für die X und Y Koordinate
+  mouseX = signal(0);
+  mouseY = signal(0);
+
+  // Der HostListener "hört" auf Bewegungen im gesamten Dokument
+  @HostListener('document:mousemove', ['$event'])
+  onMouseMove(event: MouseEvent) {
+    // Wir setzen die neuen Werte in die Signals
+    this.mouseX.set(event.clientX);
+    this.mouseY.set(event.clientY);
+  }
 }
