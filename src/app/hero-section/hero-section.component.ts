a657cc9ffdc882d85e68fad3214cf5ac0f4dfd92
Sebastian Böhme <sebastianboehme92@gmail.com>
2026-03-03 15:26:18 +0100

    translated hero-section and uses an @for loop in the marquee to replace the muliple hardcoded html in this section.

--- src/app/hero-section/hero-section.component.ts
+++ src/app/hero-section/hero-section.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { TranslateModule } from '@ngx-translate/core';
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-hero-section',
@@ -10,4 +10,12 @@ import { TranslateModule } from '@ngx-translate/core';
 })
 export class HeroSectionComponent {
   email: string = 'sebastianboehme.dev@gmail.com';
+
+  marqueeItems: string[] = [
+    'HERO.LOCATION',
+    'HERO.OPEN_FOR_WORK',
+    'HERO.REMOTELY',
+  ];
+
+  constructor(public translate: TranslateService) {}
 }
