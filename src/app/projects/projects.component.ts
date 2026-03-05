69b7dbdfc71e21e8a157baf7ea5488a2787157f9
Sebastian Böhme <sebastianboehme92@gmail.com>
2026-03-05 14:05:21 +0100

    translated skills section via ngx translate pipes

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
 
 interface Tool {
   name: string;
@@ -8,7 +9,7 @@ interface Tool {
 interface Project {
   number: string;
   name: string;
-  description: string;
+  descriptionKey: string;
   tools: Tool[];
   short: string;
   image: string;
@@ -19,70 +20,90 @@ interface Project {
 @Component({
   selector: 'app-projects',
   standalone: true,
-  imports: [],
+  imports: [TranslateModule],
   templateUrl: './projects.component.html',
-  styleUrl: './projects.component.scss'
+  styleUrl: './projects.component.scss',
 })
 export class ProjectsComponent {
-
   hoveredProject: Project | null = null;
   hoveredIndex: number = -1;
   selectedProject: Project | null = null;
 
+  constructor(public translate: TranslateService) {}
 
-openOverlay(project: Project) {
-  this.selectedProject = project;
-  document.body.style.overflow = 'hidden';
-}
+  openOverlay(project: Project) {
+    this.selectedProject = project;
+    document.body.style.overflow = 'hidden';
+  }
 
-closeOverlay() {
-  this.selectedProject = null;
-  document.body.style.overflow = '';
-}
+  closeOverlay() {
+    this.selectedProject = null;
+    document.body.style.overflow = '';
+  }
 
-showNextProject(event: Event): void {
-  event.stopPropagation();
+  showNextProject(event: Event): void {
+    event.stopPropagation();
 
-  if (!this.selectedProject) return;
+    if (!this.selectedProject) return;
 
-  const currentIndex = this.projects.findIndex(
-    (project) => project.name === this.selectedProject!.name
-  );
+    const currentIndex = this.projects.findIndex(
+      (project) => project.name === this.selectedProject!.name,
+    );
 
-  const nextIndex = (currentIndex + 1) % this.projects.length;
-  this.selectedProject = this.projects[nextIndex];
-}
+    const nextIndex = (currentIndex + 1) % this.projects.length;
+    this.selectedProject = this.projects[nextIndex];
+  }
 
   projects: Project[] = [
     {
       number: '01',
       name: 'JOIN',
-      description: 'Ein kollaboratives To-do-Tool nach dem Vorbild von Trello - Aufgaben erstellen, zuweisen und per Drag & Drop nach Status sortieren sowie Kontakte erstellen, bearbeiten und löschen.',
+      descriptionKey: 'PROJECTS.JOIN_DESC',
       tools: [
-        { name: 'HTML', icon: '../../assets/img/projects-icons/html-icon-green.svg' },
-        { name: 'CSS', icon: '../../assets/img/projects-icons/css-icon-green.svg' },
-        { name: 'JavaScript', icon: '../../assets/img/projects-icons/javascript-icon-green.svg' },
-        { name: 'Firebase', icon: '../../assets/img/projects-icons/firebase-icon-green.svg' }
+        {
+          name: 'HTML',
+          icon: '../../assets/img/projects-icons/html-icon-green.svg',
+        },
+        {
+          name: 'CSS',
+          icon: '../../assets/img/projects-icons/css-icon-green.svg',
+        },
+        {
+          name: 'JavaScript',
+          icon: '../../assets/img/projects-icons/javascript-icon-green.svg',
+        },
+        {
+          name: 'Firebase',
+          icon: '../../assets/img/projects-icons/firebase-icon-green.svg',
+        },
       ],
       short: 'JS',
       image: '../../assets/img/projects-icons/join-image.png',
       link: '',
-      git: 'https://github.com/Pe3et/Join'
+      git: 'https://github.com/Pe3et/Join',
     },
     {
       number: '02',
       name: 'El Pollo Loco',
-      description: 'Ein kleines, lustiges 2D Spiel mit einem Objektorientierten Ansatz. Begleite Pepe bei seinem Kampf gegen einen Hühneraufstand. Renne, springe und bewirf die verrückten Hennen mit Tabascoflaschen',
+      descriptionKey: 'PROJECTS.POLLO_DESC',
       tools: [
-        { name: 'HTML', icon: '../../assets/img/projects-icons/html-icon-green.svg' },
-        { name: 'CSS', icon: '../../assets/img/projects-icons/css-icon-green.svg' },
-        { name: 'JavaScript', icon: '../../assets/img/projects-icons/javascript-icon-green.svg' },
+        {
+          name: 'HTML',
+          icon: '../../assets/img/projects-icons/html-icon-green.svg',
+        },
+        {
+          name: 'CSS',
+          icon: '../../assets/img/projects-icons/css-icon-green.svg',
+        },
+        {
+          name: 'JavaScript',
+          icon: '../../assets/img/projects-icons/javascript-icon-green.svg',
+        },
       ],
       short: 'JS',
       image: '../../assets/img/projects-icons/el-pollo-loco-image.png',
       link: '',
-      git: 'https://github.com/SebastianBoehme/Pollo-Loco'
-    }
+      git: 'https://github.com/SebastianBoehme/Pollo-Loco',
+    },
   ];
-
 }
