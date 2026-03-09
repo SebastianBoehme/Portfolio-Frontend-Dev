4f07177920773f71e26112ee08012d93dda47b82
Sebastian Böhme <sebastianboehme92@gmail.com>
2026-03-09 16:09:06 +0100

    Translated footer section

--- src/app/shared/footer/footer.component.ts
+++ src/app/shared/footer/footer.component.ts
@@ -1,15 +1,18 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterModule } from '@angular/router'; 
+import { RouterModule } from '@angular/router';
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [CommonModule, RouterModule], 
+  imports: [CommonModule, RouterModule, TranslateModule],
   templateUrl: './footer.component.html',
-  styleUrl: './footer.component.scss'
+  styleUrl: './footer.component.scss',
 })
 export class FooterComponent {
   email: string = 'sebastianboehme.dev@gmail.com';
   currentYear: number = new Date().getFullYear();
+
+  constructor(public translate: TranslateService) {}
 }
