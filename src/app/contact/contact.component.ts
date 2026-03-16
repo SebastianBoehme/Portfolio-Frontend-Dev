a3db11d56399d53a849c177090998e3b3383da45
Sebastian Böhme <sebastianboehme92@gmail.com>
2026-03-16 15:29:13 +0100

    implented Email Validation.

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, NgForm } from '@angular/forms';
 import { TranslateModule, TranslateService } from '@ngx-translate/core';
 
 @Component({
@@ -18,7 +18,9 @@ export class ContactComponent {
     message: '',
   };
 
-  onSubmit() {
-    console.log(this.contactData);
+  onSubmit(ngForm: NgForm) {
+    if (ngForm.valid && ngForm.submitted) {
+      console.log(this.contactData);
+    }
   }
 }
