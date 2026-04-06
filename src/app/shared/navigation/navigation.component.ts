af3574cdb11165e583ab889c712ffc12ad1bbe51
Sebastian Böhme <sebastianboehme92@gmail.com>
2026-04-06 14:27:54 +0200

    completed responsive design of navigation.component including fill burger menu and overlay.

--- src/app/shared/navigation/navigation.component.ts
+++ src/app/shared/navigation/navigation.component.ts
@@ -6,14 +6,34 @@ import { TranslateModule, TranslateService } from '@ngx-translate/core';
   standalone: true,
   imports: [TranslateModule],
   templateUrl: './navigation.component.html',
-  styleUrl: './navigation.component.scss',
+  styleUrl: './navigation.component.scss', // Beachte: .scss bleibt
 })
 export class NavigationComponent {
+  isMenuOpen: boolean = false;
+
   constructor(public translate: TranslateService) {}
 
   switchLanguage() {
     const current = this.translate.getCurrentLang() || 'de';
     const nextLang = current === 'de' ? 'en' : 'de';
     this.translate.use(nextLang);
+  }
+
+  toggleMenu() {
+    this.isMenuOpen = !this.isMenuOpen;
+    this.updateScrollLock();
+  }
+
+  closeMenu() {
+    this.isMenuOpen = false;
+    this.updateScrollLock();
+  }
+
+  private updateScrollLock() {
+    if (this.isMenuOpen) {
+      document.body.style.overflow = 'hidden';
+    } else {
+      document.body.style.overflow = 'auto';
+    }
   }
 }
